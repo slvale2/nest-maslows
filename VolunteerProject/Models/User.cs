e0a05fee2514a1e4cdb5be7f1ebfbcc4b78f8b43
Foster Stule <camodude5@yahoo.com>
2018-07-11 10:01:06 -0400

    added Event Class baseline

--- VolunteerProject/Models/User.cs
+++ VolunteerProject/Models/User.cs
@@ -12,6 +12,12 @@ namespace VolunteerProject.Models
 		public string Password { get; set; }
 		public string Firstname { get; set; }
 		public string Lastname { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+		public bool IsVolunteer { get; set; }
+		public bool IsAdmin { get; set; }
+		public int Hours { get; set; }
 
+		public User() { }
 	}
 }
