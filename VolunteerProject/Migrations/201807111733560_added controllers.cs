f2cc3955865a67cfe5094c2ff9118d3f7de7c86a
Foster Stule <camodude5@yahoo.com>
2018-07-11 13:34:58 -0400

    did a migration for the creation of the two controllers.

---
+++ VolunteerProject/Migrations/201807111733560_added controllers.cs
@@ -0,0 +1,16 @@
+namespace VolunteerProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedcontrollers : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
