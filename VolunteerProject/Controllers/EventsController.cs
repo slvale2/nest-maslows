bd41554955720cd2a52a2d3c65df4a581ffb8e70
Foster Stule <camodude5@yahoo.com>
2018-07-11 13:33:32 -0400

    added two controllers for the user and event class with some basic methods.

---
+++ VolunteerProject/Controllers/EventsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VolunteerProject.Utility;
+using VolunteerProject.Models;
+
+namespace VolunteerProject.Controllers
+{
+    public class EventsController : ApiController
+    {
+		private VolunteerContext db = new VolunteerContext();
+
+		[HttpGet]
+		[ActionName("Get")]
+		public JsonResponse Get(int? id)
+		{
+			if (id == null)
+				return new JsonResponse { Error = "null", Message = "Id is a null value", Result = "failed" };
+			Event @event = db.Events.Find(id);
+			return new JsonResponse { Data = @event };
+		}
+
+    }
+}
---
+++ VolunteerProject/Controllers/UsersController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VolunteerProject.Models;
+using VolunteerProject.Utility;
+
+namespace VolunteerProject.Controllers
+{
+    public class UsersController : ApiController
+    {
+		private VolunteerContext db = new VolunteerContext();
+
+		[HttpGet]
+		[ActionName("Get")]
+		public JsonResponse Get(int? id) {
+			if (id == null)
+				return new JsonResponse { Message = "Null Value", Error = "Null", Result = "failed" };
+			User u = db.Users.Find(id);
+			return new JsonResponse { Data = u };
+		}
+		[HttpGet]
+		[ActionName("List")]
+		public JsonResponse List() {
+			List<User> users = db.Users.ToList();
+			return new JsonResponse { Data = users };
+		}
+
+		[HttpPost]
+		[ActionName("Add")]
+		public JsonResponse Add(User user) {
+			if (user == null)
+				return new JsonResponse { Error = "null", Message = "Null user", Result = "Failed" };
+			if (!ModelState.IsValid)
+				return new JsonResponse { Error = "not valid", Message = "non valid user", Result = "failed" };
+			db.Users.Add(user);
+			db.SaveChanges();
+			return new JsonResponse();
+		}
+
+		[HttpPost]
+		[ActionName("Change")]
+		public JsonResponse Change(User user) {
+			if (user == null)
+				return new JsonResponse { Error = "null", Message = "Null user", Result = "Failed" };
+			if (!ModelState.IsValid)
+				return new JsonResponse { Error = "not valid", Message = "non valid user", Result = "failed" };
+			User u = db.Users.Find(user.Id);
+			u.Email = user.Email;
+			u.Firstname = user.Firstname;
+			u.Lastname = user.Lastname;
+			u.Password = user.Password;
+			u.Username = user.Username;
+			u.IsAdmin = user.IsAdmin;
+			u.IsVolunteer = user.IsVolunteer;
+			u.Hours = user.Hours;
+			u.Phone = user.Phone;
+			db.SaveChanges();
+			return new JsonResponse();
+		}
+
+		[HttpPost]
+		[ActionName("Delete")]
+		public JsonResponse Delete(int? id)
+		{
+			if (id == null)
+				return new JsonResponse { Error = "null", Message = "Id is null", Result = "failed" };
+			User u = db.Users.Find(id);
+			db.Users.Remove(u);
+			db.SaveChanges();
+			return new JsonResponse();
+		}
+
+		[HttpPost]
+		[ActionName("Promote")]
+		public JsonResponse PromoteToAdmin(User user, int? id) {
+			if (user == null)
+				return new JsonResponse { Error = "null", Message = "Null user", Result = "Failed" };
+			if (!ModelState.IsValid)
+				return new JsonResponse { Error = "not valid", Message = "non valid user", Result = "failed" };
+			if (id == null)
+				return new JsonResponse { Error = "null", Message = "Null id", Result = "failed" };
+			if (user.IsAdmin == false)
+				return new JsonResponse { Error = "not high clearance", Message = "User does not have the authority", Result = "failed" };
+			User u = db.Users.Find(id);
+			u.IsAdmin = true;
+			db.SaveChanges();
+			return new JsonResponse();
+		}
+    }
+}
