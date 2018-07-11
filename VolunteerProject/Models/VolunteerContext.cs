2d5fd10457b2e57427607486b52bbc5b5313b8b0
Foster Stule <camodude5@yahoo.com>
2018-07-11 10:33:15 -0400

    Added a context to access the database

---
+++ VolunteerProject/Models/VolunteerContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace VolunteerProject.Models
+{
+	public class VolunteerContext : DbContext
+	{
+		public VolunteerContext() : base() { }
+
+		public DbSet<User> Users { get; set; }
+		public DbSet<Event> Events { get; set; }
+	}
+}
